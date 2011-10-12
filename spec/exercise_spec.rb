a49d6fc62c06c83f398ee3c533acfe0d3d95f191
Thomas Utterback <timba@noisycrow.com>
2011-10-11 20:25:55 -0700

    Test fails and you're my only hope.

---
+++ spec/exercise_spec.rb
@@ -0,0 +1,20 @@
+describe "Strings" do
+	context "when calling strip" do
+		it "should remove all whitespace from the beginning and the end of the string" do
+			lyrics = "    Hello, is it me you're looking for I can see it in your eyes    "
+			lyrics.strip.should eq "Hello, is it me you're looking for I can see it in your eyes"
+		end
+	end
+end
+
+
+describe "Int Next" do
+	context "with an integer" do
+		it "should add to the integer" do
+		
+		x = 3
+		x.next.next.next.even?.should eq 6.0
+		
+		end
+	end
+end
